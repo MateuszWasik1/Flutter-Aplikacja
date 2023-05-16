762b0a8f0e374491bd6e85280a6b692515f3e665
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2023-05-16 18:13:48 +0200

    account pt1

--- apki_mobilne/lib/account.dart
+++ apki_mobilne/lib/account.dart
@@ -1,22 +1,153 @@
+import 'dart:convert';
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-const int itemCount = 20;
+Future<User> fetchUser() async {
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users/1'));
 
-class AccountPage extends StatelessWidget {
+  if (response.statusCode == 200) {
+    return User.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load user');
+  }
+}
+
+class User {
+  final int id;
+  final String name;
+  final String username;
+  final String email;
+  final Object address;
+  final String phone;
+  final String website;
+
+  const User({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.email,
+    required this.address,
+    required this.phone,
+    required this.website,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      name: json['name'],
+      username: json['username'],
+      email: json['email'],
+      address: json['address'],
+      phone: json['phone'],
+      website: json['website'],
+    );
+  }
+}
+
+class AccountPage extends StatefulWidget {
   const AccountPage({Key? key}) : super(key: key);
 
   @override
+  State<AccountPage> createState() => _AccountPageState();
+}
+
+class _AccountPageState extends State<AccountPage> {
+  late Future<User> futureUser;
+
+  @override
+  void initState() {
+    super.initState();
+    futureUser = fetchUser();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: itemCount,
-      itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          title: Text('Item ${(index + 1)}'),
-          leading: const Icon(Icons.percent),
-          trailing: const Icon(Icons.access_alarm_outlined),
-          onTap: () => debugPrint('Item ${(index + 1)}'),
-        );
-      },
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: FutureBuilder<User>(
+            future: futureUser,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: CircleAvatar(
+                        radius: 50.0,
+                        backgroundImage: const ExactAssetImage(
+                            'images/profile.jpg'),
+                      ),
+                    ),
+                    const SizedBox(height: 16.0),
+                    const Text(
+                      'Name:',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(snapshot.data!.name),
+                    const SizedBox(height: 8.0),
+                    const Text(
+                      'Username:',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(snapshot.data!.username),
+                    const SizedBox(height: 8.0),
+                    const Text(
+                      'Email:',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(snapshot.data!.email),
+                    const SizedBox(height: 8.0),
+                    const Text(
+                      'Address:',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(snapshot.data!.address
+                        .toString()), // Replace with appropriate widget for address
+                    const SizedBox(height: 8.0),
+                    const Text(
+                      'Phone:',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(snapshot.data!.phone),
+                    const SizedBox(height: 8.0),
+                    const Text(
+                      'Website:',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(snapshot.data!.website),
+                  ],
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+
+              return const CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
     );
   }
 }
